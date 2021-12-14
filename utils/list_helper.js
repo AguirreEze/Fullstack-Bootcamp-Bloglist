512637f6e905994b68386068a580edc807f7dfbc
AguirreEze <aguirre.eze95@gmail.com>
2021-12-14 12:09:11 -0300

    Feat BlogList tests Step 3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -7,7 +7,22 @@ const totalLikes = (list) => {
   return list.map(e => e.likes).reduce((a, b) => a + b)
 }
 
+const favoriteBlog = (list) => {
+  if (list.length === 0) return undefined
+  const mostLikes = Math.max(...list.map(e => e.likes))
+  const mostLiked = list.find(e => e.likes === mostLikes)
+
+  return (
+    {
+      title: mostLiked.title,
+      author: mostLiked.author,
+      likes: mostLiked.likes
+    }
+  )
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
